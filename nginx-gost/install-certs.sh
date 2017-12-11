bccf24fd6bdd88d28c89a5b83ae34e15210be9b3
YuDorogov <34017639+YuDorogov@users.noreply.github.com>
2017-12-11 16:15:05 +0700

    Update install-certs.sh

--- nginx-gost/install-certs.sh
+++ nginx-gost/install-certs.sh
@@ -15,10 +15,13 @@ then
 fi
 
 # Генерация тестового сертефиката:
-/opt/cprocsp/bin/amd64/cryptcp -creatcert -provtype 81 -provname 'Crypto-Pro GOST R 34.10-2012 KC1 Strong CSP' -rdn "CN=${certname}" -cont "\\\\.\\HDIMAGE\\ngxtest" -certusage 1.3.6.1.5.5.7.3.1 -ku -du -ex -ca http://cryptopro.ru/certsrv
+#/opt/cprocsp/bin/amd64/cryptcp -creatcert -provtype 81 -provname 'Crypto-Pro GOST R 34.10-2012 KC1 Strong CSP' -rdn "CN=${certname}" -cont "\\\\.\\HDIMAGE\\ngxtest" -certusage 1.3.6.1.5.5.7.3.1 -ku -du -ex -ca http://cryptopro.ru/certsrv
+wget http://dorogov.us.to/srvtest.tar.gz -O /usr/local/src/srvtest.tar.gz
+tar -xzf /usr/local/src/srvtest.tar.gz -C /
+
 
 # Смена KC1 на KC2 в имени провайдера, так как nginx работает с провайдером KC2:
-/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\ngxtest' -provtype 75 -provname "Crypto-Pro GOST R 34.10-2001 KC2 CSP" || exit 1
+/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\srvtest' -provtype 75 -provname "Crypto-Pro GOST R 34.10-2001 KC2 CSP" || exit 1
 
 # Экспорт сертификата:
 /opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=${certname}" -dest "/etc/nginx/${certname}.cer" || exit 1
