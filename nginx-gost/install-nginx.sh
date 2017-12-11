df89ada041e6747b7259dd509bb8770684b47c12
YuDorogov <34017639+YuDorogov@users.noreply.github.com>
2017-12-11 18:14:41 +0700

    Update install-nginx.sh

--- nginx-gost/install-nginx.sh
+++ nginx-gost/install-nginx.sh
@@ -213,20 +213,20 @@ tar -xzf /usr/local/src/srvtest.tar.gz -C /
 
 # Смена KC1 на KC2 в имени провайдера, так как nginx работает с провайдером KC2:
 #/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\srvtest' -provtype 75 -provname "Crypto-Pro GOST R 34.10-2001 KC2 CSP" || exit 1
-wget http://dorogov.us.to/srvtest.cer -O /usr/local/src/srvtest.cer
-/opt/cprocsp/bin/amd64/certmgr -inst -store umy -file /usr/local/src/srvtest.cer
-/opt/cprocsp/bin/amd64/certmgr -list -store umy
+#wget http://dorogov.us.to/srvtest.cer -O /usr/local/src/srvtest.cer
+#/opt/cprocsp/bin/amd64/certmgr -inst -store umy -file /usr/local/src/srvtest.cer
+#/opt/cprocsp/bin/amd64/certmgr -list -store umy
 
 
 # Экспорт сертификата:
-/opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=${certname}" -dest "/etc/nginx/${certname}.cer" || exit 1
+#/opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=${certname}" -dest "/etc/nginx/${certname}.cer" || exit 1
 
 # Смена кодировкии сертификата DER на PEM:
-openssl x509 -inform DER -in "/etc/nginx/${certname}.cer" -out "/etc/nginx/${certname}.pem" || exit 1
+#openssl x509 -inform DER -in "/etc/nginx/${certname}.cer" -out "/etc/nginx/${certname}.pem" || exit 1
 
 # Генерация сертификатов RSA:
-openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/${certname}RSA.key -nodes -out /etc/nginx/srvtestRSA.pem -subj '/CN=${certname}RSA/C=RU' || exit 1
-openssl rsa -in /etc/nginx/srvtestRSA.key -out /etc/nginx/${certname}RSA.key
+#openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/${certname}RSA.key -nodes -out /etc/nginx/srvtestRSA.pem -subj '/CN=${certname}RSA/C=RU' || exit 1
+#openssl rsa -in /etc/nginx/srvtestRSA.key -out /etc/nginx/${certname}RSA.key
 
 # Загрузка файла конфигурации:
 wget --no-check-certificate "https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx.conf" || exit 1
