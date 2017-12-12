78a1bdfe56a2fba38e39aeabf7c819958bbeb07f
YuDorogov <34017639+YuDorogov@users.noreply.github.com>
2017-12-12 11:31:01 +0700

    Update install-nginx.sh

--- nginx-gost/install-nginx.sh
+++ nginx-gost/install-nginx.sh
@@ -9,7 +9,7 @@ certname='srvtest'
 revision_openssl="164682"
 pcre_ver="pcre-8.41"
 zlib_ver="zlib-1.2.11"
-csp=/usr/local/src/linux-amd64.tgz
+csp=/usr/local/src/linux-amd64R3.tgz
 # Версия nginx для загрузки с github
 nginx_branch="stable-1.12"
 no_exec=false
@@ -212,21 +212,21 @@ tar -xzf /usr/local/src/srvtest.tar.gz -C /
 
 
 # Смена KC1 на KC2 в имени провайдера, так как nginx работает с провайдером KC2:
-#/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\srvtest' -provtype 75 -provname "Crypto-Pro GOST R 34.10-2001 KC2 CSP" || exit 1
+/opt/cprocsp/bin/amd64/certmgr -inst -store uMy -cont '\\.\HDIMAGE\srvtest' -provtype 75 -provname "Crypto-Pro GOST R 34.10-2001 KC2 CSP" || exit 1
 #wget http://dorogov.us.to/srvtest.cer -O /usr/local/src/srvtest.cer
 #/opt/cprocsp/bin/amd64/certmgr -inst -store umy -file /usr/local/src/srvtest.cer
 #/opt/cprocsp/bin/amd64/certmgr -list -store umy
 
 
 # Экспорт сертификата:
-#/opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=${certname}" -dest "/etc/nginx/${certname}.cer" || exit 1
+/opt/cprocsp/bin/amd64/certmgr -export -cert -dn "CN=${certname}" -dest "/etc/nginx/${certname}.cer" || exit 1
 
 # Смена кодировкии сертификата DER на PEM:
-#openssl x509 -inform DER -in "/etc/nginx/${certname}.cer" -out "/etc/nginx/${certname}.pem" || exit 1
+openssl x509 -inform DER -in "/etc/nginx/${certname}.cer" -out "/etc/nginx/${certname}.pem" || exit 1
 
 # Генерация сертификатов RSA:
-#openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/${certname}RSA.key -nodes -out /etc/nginx/srvtestRSA.pem -subj '/CN=${certname}RSA/C=RU' || exit 1
-#openssl rsa -in /etc/nginx/srvtestRSA.key -out /etc/nginx/${certname}RSA.key
+openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/${certname}RSA.key -nodes -out /etc/nginx/srvtestRSA.pem -subj '/CN=${certname}RSA/C=RU' || exit 1
+openssl rsa -in /etc/nginx/srvtestRSA.key -out /etc/nginx/${certname}RSA.key
 
 # Загрузка файла конфигурации:
 wget --no-check-certificate "https://raw.githubusercontent.com/fullincome/scripts/master/nginx-gost/nginx.conf" || exit 1
